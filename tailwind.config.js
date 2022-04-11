0402e9b609e5cb41786bd708ac44d3b8fe356431
Chris Clynes <chrisclynes@live.com>
2022-04-10 23:32:12 -0400

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  
+  darkMode: false,
+  content: ['./src/**/*.{js, jsx, ts, tsx}', './public/index.html'],
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
